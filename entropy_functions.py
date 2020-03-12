45339b1ece66b4d88b94ec16b162d55d70b9f6c8
Zexun <Z.Chen3@exeter.ac.uk>
2020-03-12 23:04:02 +0000

    improve functions

--- entropy_functions.py
+++ entropy_functions.py
@@ -48,6 +48,7 @@ def entropy(seq, lambdas=False):
         return L
     return (1.0*N/sum(L)) * log2(N)
 
+
 # Modified by Zexun
 # Since the LZ-entropy estimation only coverge when the length is large, so we add one more arg for LZ-entropy function
 def LZ_entropy(seq, e=100, lambdas=False):
@@ -65,7 +66,7 @@ def LZ_entropy(seq, e=100, lambdas=False):
         return np.nan
     else:
         L = []
-        for i,w in enumerate(seq):
+        for i, w in enumerate(seq):
             seen = True
             prevSeq = " %s " % " ".join(seq[0:i])
             c = i
@@ -92,13 +93,13 @@ def cross_entropy(W1, W2, PTs, lambdas=False):
     lenW1 = len(W1)
     lenW2 = len(W2)
     L = []
-    for j,(wj,i) in enumerate( zip(W2,PTs) ):
+    for j, (wj, i) in enumerate(zip(W2, PTs)):
         seen = True
         prevW1 = " %s " % " ".join( W1[:i] )
         c = j
         while seen and c < lenW2:
             c += 1
-            seen = (" %s " % " ".join(W2[j:c]) in prevW1 )
+            seen = (" %s " % " ".join(W2[j:c]) in prevW1)
         l = c - j
         L.append(l)
     
@@ -106,3 +107,36 @@ def cross_entropy(W1, W2, PTs, lambdas=False):
         return L
     return (1.0*lenW2/sum(L)) * log2(lenW1)
 
+
+# Modified by Zexun
+# Since the LZ-crossentropy estimation only coverge when the length is large, so we add one more arg for LZ-entropy
+# function
+def LZ_cross_entropy(W1, W2, PTs, lambdas=False, e=100):
+    """Find the cross entropy H_cross(W2|W1), how many bits we would need to
+    encode the data in W2 using the information in W1. W1 and W2 are lists of
+    strings, PTs is a list of integers with the same length as W2 denoting the
+    relative time ordering of W1 vs. W2. These integers tell us the position
+    PTs[x] = i in W1 such that all symbols in W1[:i] occurred before the x-th
+    word in W2.
+    """
+
+    lenW1 = len(W1)
+    lenW2 = len(W2)
+
+    if lenW1 < e | lenW2 < e:
+        return np.nan
+    else:
+        L = []
+        for j, (wj, i) in enumerate(zip(W2, PTs)):
+            seen = True
+            prevW1 = " %s " % " ".join(W1[:i])
+            c = j
+            while seen and c < lenW2:
+                c += 1
+                seen = (" %s " % " ".join(W2[j:c]) in prevW1)
+            l = c - j
+            L.append(l)
+
+        if lambdas:
+            return L
+        return (1.0 * lenW2 / sum(L)) * log2(lenW1)
--- meetup_process.py
+++ meetup_process.py
@@ -3,11 +3,13 @@
 
 # entropy_functions.py
 # (c) Zexun Chen, 2020-03-10
+# sxtpy2010@gmail.comf
 
 import pandas as pd
 import numpy as np
 import mpmath
-from entropy_functions import shannon_entropy, entropy, cross_entropy, LZ_entropy
+from entropy_functions import shannon_entropy, entropy, \
+    cross_entropy, LZ_entropy, LZ_cross_entropy
 
 
 # As required by algorithm, N should be large, we set e as the threshold of N.
@@ -76,25 +78,52 @@ class MeetupStrategy:
         Return:
             float, cross entropy for a pair of ego and alters
         """
-        alters_Lmax = np.amax(alters_L, axis=0)
+        # remove all the alters_L with nan
+        clean_alters_L = [x for x in alters_L if ~np.isnan(x).all()]
+        alters_Lmax = np.amax(clean_alters_L, axis=0)
         return (1.0 * length_ego / sum(alters_Lmax)) * np.log2(ave_length)
 
     @staticmethod
     def weight(ego_L, alter_L=None):
-        """ Public method, compute how important of alter for ego"""
-        # TODO: How to define weight is a problem
-        # # Definition 1, which is cloe to the paper's definition
-        # if alter_L is None:
-        #     return len(ego_L)
-        # else:
-        #     # count how many elements of ego_L is in alter_L
-        #     return sum(x in alter_L for x in ego_L)
-        # # Definition 2, count how many times of match length of A find in B
+        """ Public method, compute how important of alter for ego
+        Args:
+            ego_L: list, match length of ego
+            alter_L: list, match length of alter compared with ego
+
+        Return: int
+            weight of this alter among all the alters compared with ego
+        """
         if alter_L is None:
-            return sum(map(lambda x : x>1, ego_L))
+            alter_L = ego_L
+
+        if np.isnan(ego_L).all() | np.isnan(alter_L).all():
+            return np.nan
         else:
+            # TODO: How to define weight is a problem
+            # Definition 1, which is cloe to the paper's definition
+            # return sum(x in alter_L for x in ego_L)
+
+            # # Definition 2, count how many times of match length of A find in B
             # count how many elements of ego_L is in alter_L
-            return sum(map(lambda x : x>1, alter_L))
+            return sum(map(lambda x: x > 1, alter_L))
+
+    def __ave(self, lenB, wB):
+        """ Compute the average legnth of B
+        Args:
+            lenB: list, a list of the length of placeid of B, it might be nan
+            wB: list, a list of the weight of B, it might be nan
+        """
+        # since wB is compute by LZ algorithm, it maybe nan
+        # lenB is the length of series
+        if np.isnan(wB).all() | np.isnan(lenB).all():
+            # another condition is that the product is nan
+            # but according to our weight definition,
+            # if lenB is nan, wB must be nan
+            return np.nan
+        else:
+            alter_length = np.array(lenB, dtype=np.float64)
+            alter_wb = np.array(wB, dtype=np.float64)
+            return np.nanmean(alter_length * alter_wb) / np.nansum(alter_wb)
 
     def __cross_entropy_element(self, ego_time, ego_placeid, ego_L, alter, alters, L, wb, length_alters):
         """ Private method (recursive structure): compute cross entropy related to statistics
@@ -123,19 +152,21 @@ class MeetupStrategy:
         """Be careful: W1 in cross_entropy is B in the paper, W2 is cross_entropy is A in the paper """
         # so we need to get the relative time order of ego in alter (abosulte position of ego+alter)
         # for function cross_entropy, we need to have PTs
+        # here we use LZ-cross entropy, which requires the length at least self.epsilon
         total_time = sorted(ego_time + alter_time)
         PTs = [total_time.index(x) for x in ego_time]
 
         """ function cross_entropy can return L, as defintion of cumulative cross entropy, we need to get max """
         # compute cross entropy with only this alter
         """ For alter"""
-        CE_alter = cross_entropy(alter_placeid, ego_placeid, PTs)
+        CE_alter = LZ_cross_entropy(alter_placeid, ego_placeid, PTs, e=self.epsilon)
         Pi_alter = getPredictability(length_ego, CE_alter, e=self.epsilon)
 
         """ For all above alters """
         # Obtain the basic information to extend L, wb, length_alters
         # obtain the cross-parsed match length for this ego-alter pair
-        L[alterid] = cross_entropy(alter_placeid, ego_placeid, PTs, lambdas=True)
+        L[alterid] = LZ_cross_entropy(alter_placeid, ego_placeid, PTs, \
+                                      lambdas=True, e=self.epsilon)
         wb[alterid] = self.weight(ego_L, L[alterid])
         # length of alter placeid
         length_alters[alterid] = length_alter
@@ -145,9 +176,7 @@ class MeetupStrategy:
         alters_length = length_alters[:alterid + 1]
         wb_length = wb[:alterid + 1]
         # average lengths
-        # use float64 to get Inf if the sum is zero
-        ave_length = np.mean(np.array(alters_length, dtype=np.float64) * \
-                             np.array(wb_length, dtype=np.float64) / sum(wb_length))
+        ave_length = self.__ave(alters_length, wb_length)
         # CCE for all above alters
         CCE_alters = self.cross_entropy_pair(length_ego, alters_L, ave_length)
         Pi_alters = getPredictability(length_ego, CCE_alters, e=self.epsilon)
@@ -157,8 +186,7 @@ class MeetupStrategy:
         ego_alter_L = [ego_L, L[alterid]]
         bi_length = np.array([length_alters[alterid], length_ego], dtype=np.float64)
         bi_weight = np.array([wb[alterid], self.weight(ego_L)], dtype=np.float64)
-        # use np.float64 to get Inf to overcome the error when the sum is zero
-        ave_length = np.mean(bi_length * bi_weight / sum(bi_weight))
+        ave_length = self.__ave(bi_length, bi_weight)
         CCE_ego_alter = self.cross_entropy_pair(length_ego, ego_alter_L, ave_length)
         Pi_ego_alter = getPredictability(length_ego, CCE_ego_alter, e=self.epsilon)
 
@@ -167,10 +195,7 @@ class MeetupStrategy:
         alters_L.append(ego_L)
         alters_length.append(length_ego)
         ego_alters_weight = wb[:alterid + 1] + [self.weight(ego_L)]
-        # use np.float64 to get Inf to overcome the error when the sum is zero
-        ave_length = np.mean(np.float64(np.array(alters_length, dtype=np.float64) * \
-                                        np.array(ego_alters_weight, dtype=np.float64)) \
-                             / sum(ego_alters_weight))
+        ave_length = self.__ave(alters_length, ego_alters_weight)
         CCE_ego_alters = self.cross_entropy_pair(length_ego, alters_L, ave_length)
         Pi_ego_alters = getPredictability(length_ego, CCE_ego_alters, e=self.epsilon)
 
@@ -198,7 +223,6 @@ class MeetupStrategy:
         N_alters = len(alters)
 
         ego_L = LZ_entropy(ego_placeid, e=self.epsilon, lambdas=True)
-
         # initial space
         L = [None] * N_alters
         wb = [None] * N_alters
@@ -257,7 +281,7 @@ class MeetupStrategy:
         if end is None:
             end = len(self.userlist)
 
-        ego_time, length_ego_uni, length_ego, ego_placeid = zip(*[self.ego_info(ego) \
+        ego_time, length_ego_uni, length_ego, ego_placeid = zip(*[self._extract_info(ego) \
                                                                   for ego in self.userlist[start:end]])
         N = end - start
         ego_LZ_entropy = [LZ_entropy(ego_placeid[i], e=self.epsilon) \
@@ -265,8 +289,12 @@ class MeetupStrategy:
         Pi_ego = [getPredictability(length_ego[i], ego_LZ_entropy[i], e=self.epsilon) \
                   for i in range(N)]
         ego_log2 = list(length_ego_uni)
-        df_ego = [self.userlist[start:end], ego_log2, ego_LZ_entropy, Pi_ego]
-        self.ego_stats = pd.DataFrame(df_ego, columns=['userid_x', 'ego_info', 'LZ_entropy', 'Pi'])
+        df_ego = {'userid_x': self.userlist[start:end],
+                  'ego_info': ego_log2,
+                  'LZ_entropy': ego_LZ_entropy,
+                  'Pi':Pi_ego
+        }
+        self.ego_stats = pd.DataFrame(data=df_ego)
 
         if filesave:
             self.ego_stats.to_csv('user-ego-info.csv', index=False)
