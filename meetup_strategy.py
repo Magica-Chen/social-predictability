c5370582ff398592a87f77eb226c659450beab07
Magica_Chen <sxtpy2010@gmail.com>
2020-03-29 23:08:18 +0100

    add comments for geo-id functions

--- meetup_strategy.py
+++ meetup_strategy.py
@@ -763,6 +763,13 @@ class MeetupStrategy(Meetup):
 
 
 def geo2id(df, resolution=None, lat='lat', lon='lon'):
+    """ convert lat and lon to a geo-id
+    :param df: dataframe or series with geo-coordinate information
+    :param lon: longitude
+    :param lat: latitude
+    :param resolution: what the resolution used when taking geo-id
+    :return: dataframe with geo-id
+    """
     if resolution is not None:
         df = pd.DataFrame(df)
         df['lat'] = df['lat'].map(lambda x: round(x, resolution))
@@ -774,6 +781,14 @@ def geo2id(df, resolution=None, lat='lat', lon='lon'):
 
 
 def pre_processing(df_raw, min_records=200, filesave=False, geoid=False, resolution=4):
+    """ pre-processing the given dataset
+    :param df_raw: dataframe, raw dataset
+    :param min_records: the min requirement of users' records, remove all invalid users' information.
+    :param filesave: whether save the pre-processed results
+    :param geoid: whether use geo-id
+    :param resolution: when geo-id used, the resolution
+    :return: pre-processed dataframe
+    """
     df_wp = df_raw.dropna(subset=['userid', 'placeid', 'datetime'])[[
         'userid', 'placeid', 'datetime', 'lat', 'lon']]
 
