7102eec52dc3d8200f4a4f68298ae0f8e6545dff
Magica_Chen <sxtpy2010@gmail.com>
2020-04-21 23:52:47 +0100

    add gender plot function

--- visualization.py
+++ visualization.py
@@ -282,3 +282,189 @@ def num_point_plot(user_stats, name, threshold=None, partition=False,
     if figsave:
         title = name + '-point.' + format
         fig.savefig(title, bbox_inches='tight')
+
+
+def num_point_plot_gender(user_stats, name, user_stats_mixed=None, threshold=None,
+                          interval=None, l=15, w=6, mode='talk',
+                          figsave=False, format='eps'):
+    """ number of included alters vs entropy or predictability
+    :param user_stats_mixed: user_stats with mixed gender
+    :param user_stats: result dataset
+    :param name: string, currently only accept 'entropy' or 'predictability'
+    :param threshold: int, the largest number of alters included
+    :param interval: int, the interval shown in axis
+    :param l: int, long
+    :param w: int, width
+    :param mode: string, see from seaborn, available,'talk', 'notebook',
+    'paper', 'poster'.
+    :param figsave: whether the figure will be saved
+    :param format: png, eps, pdf, and so on.
+    :return: None
+    """
+    user_stats_M = user_stats[user_stats['Gender_Guesser_y'] == 'M']
+    user_stats_F = user_stats[user_stats['Gender_Guesser_y'] == 'F']
+
+    fig, ax = plt.subplots(figsize=(l, w))
+    sns.set_context(mode)
+
+    if name is 'entropy':
+        CCE_M = pd.melt(user_stats_M, id_vars=['Included Rank'],
+                        value_vars=['CCE_ego_alters', 'CCE_alters'],
+                        var_name='CCE')
+        CCE_F = pd.melt(user_stats_F, id_vars=['Included Rank'],
+                        value_vars=['CCE_ego_alters', 'CCE_alters'],
+                        var_name='CCE')
+        if user_stats_mixed is not None:
+            CCE_mixed = pd.melt(user_stats_mixed, id_vars=['Included Rank'],
+                                value_vars=['CCE_ego_alters', 'CCE_alters'],
+                                var_name='CCE')
+
+        # since F and M have same egolist, so user_stats has two rank=1, so they have the same mean
+        baseline = user_stats[user_stats['Included Rank'] == 1]['LZ_entropy'].mean()
+
+    elif name is 'predictability':
+        CCE_M = pd.melt(user_stats_M, id_vars=['Included Rank'],
+                        value_vars=['Pi_ego_alters', 'Pi_alters'],
+                        var_name='CCE')
+        CCE_F = pd.melt(user_stats_F, id_vars=['Included Rank'],
+                        value_vars=['Pi_ego_alters', 'Pi_alters'],
+                        var_name='CCE')
+        if user_stats_mixed is not None:
+            CCE_mixed = pd.melt(user_stats_mixed, id_vars=['Included Rank'],
+                                value_vars=['Pi_ego_alters', 'Pi_alters'],
+                                var_name='CCE')
+        # since F and M have same egolist, so user_stats has two rank=1, so they have the same mean
+        baseline = user_stats[user_stats['Included Rank'] == 1]['Pi'].mean()
+
+    else:
+        raise ValueError('Only available for entropy and predictability')
+
+    if threshold is None:
+        threshold = len(set(user_stats['Included Rank'].tolist()))
+
+    CCE_M['CCE'] = CCE_M.apply(lambda row: row.CCE + '-male', axis=1)
+    CCE_F['CCE'] = CCE_F.apply(lambda row: row.CCE + '-female', axis=1)
+    CCE = pd.concat([CCE_M, CCE_F])
+    CCE_legend = ['Ego only',
+                  'Male alters + ego', 'Male alters only',
+                  'Female alters + ego', 'Female alters only']
+
+    if user_stats_mixed is not None:
+        CCE_mixed['CCE'] = CCE_mixed.apply(lambda row: row.CCE + '-mixed', axis=1)
+        CCE = pd.concat([CCE, CCE_mixed])
+        CCE_legend = ['Ego only',
+                      'Male alters + ego', 'Male alters only',
+                      'Female alters + ego', 'Female alters only',
+                      'Mixed alters + ego', 'Mixed alters only']
+
+    sns.pointplot(x="Included Rank", y="value", hue='CCE',
+                  data=CCE[CCE['Included Rank'] <= threshold],
+                  ci=95, join=False, ax=ax)
+
+    ax.axhline(y=baseline, color='black', linestyle='--', label='Ego')
+
+    leg_handles = ax.get_legend_handles_labels()[0]
+
+    if interval is None:
+        interval = round(threshold / 20)
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(interval))
+    ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
+
+    if name is 'predictability':
+        vals = ax.get_yticks()
+        ax.set_yticklabels(['{:,.2%}'.format(x) for x in vals])
+        ax.legend(leg_handles, CCE_legend)
+        ax.set(xlabel='Number of included alters', ylabel='Predictability')
+    elif name is 'entropy':
+        ax.legend(leg_handles, CCE_legend, loc='upper left', bbox_to_anchor=(1.04, 1))
+        ax.set(xlabel='Number of included alters', ylabel='Cumulative Cross Entropy')
+
+    plt.show()
+
+    if figsave:
+        title = name + '-point.' + format
+        fig.savefig(title, bbox_inches='tight')
+
+
+def hist_entropy_gender(user_stats, user_stats_mixed=None, l=12, w=6, n_bins=100, mode='talk'):
+    """ Histogram plot for entropy and more
+    :param user_stats_mixed: the dataset with mixed gender
+    :param user_stats: dataset, containing all ego-alter pair info
+    :param l: int, long
+    :param w: int, width
+    :param n_bins: int, how many bins shown in the plot
+    :param mode: string, see from seaborn, available,'talk', 'notebook',
+    'paper', 'poster'.
+    :return: None
+    """
+    user_stats_M = user_stats[user_stats['Gender_Guesser_y'] == 'M']
+    user_stats_F = user_stats[user_stats['Gender_Guesser_y'] == 'F']
+
+    CumCrossEntropy_M = user_stats_M.groupby('userid_x').tail(1)['CCE_alters'].dropna()
+    CumCrossEntropyEgo_M = user_stats_M.groupby('userid_x').tail(1)['CCE_ego_alters'].dropna()
+
+    CumCrossEntropy_F = user_stats_F.groupby('userid_x').tail(1)['CCE_alters'].dropna()
+    CumCrossEntropyEgo_F = user_stats_F.groupby('userid_x').tail(1)['CCE_ego_alters'].dropna()
+
+    fig, ax = plt.subplots(figsize=(l, w))
+    sns.set_context(mode)
+
+    sns.distplot(CumCrossEntropy_M, label='Cumulative Cross Entropy: male alters only', bins=n_bins)
+    sns.distplot(CumCrossEntropyEgo_M, label='Cumulative Cross Entropy: ego + male alters', bins=n_bins)
+
+    sns.distplot(CumCrossEntropy_F, label='Cumulative Cross Entropy: female alters only', bins=n_bins)
+    sns.distplot(CumCrossEntropyEgo_F, label='Cumulative Cross Entropy: ego + female alters', bins=n_bins)
+
+    if user_stats_mixed is not None:
+        CumCrossEntropy_MF = user_stats_mixed.groupby('userid_x').tail(1)['CCE_alters'].dropna()
+        CumCrossEntropyEgo_MF = user_stats_mixed.groupby('userid_x').tail(1)['CCE_ego_alters'].dropna()
+        sns.distplot(CumCrossEntropy_MF, label='Cumulative Cross Entropy: mixed alters only', bins=n_bins)
+        sns.distplot(CumCrossEntropyEgo_MF, label='Cumulative Cross Entropy: ego + mixed alters', bins=n_bins)
+
+    # plt.title('Entropy and Cross entropy')
+    ax.set(xlabel='Entropy (bits)', ylabel='Density')
+    ax.legend(loc='upper left', bbox_to_anchor=(1.04, 1))
+    plt.show()
+
+
+def hist_pred_gender(user_stats, user_stats_mixed=None, l=12, w=6, n_bins=100, mode='talk'):
+    """ Histogram plot for predictability and theirs
+    :param user_stats_mixed: dataset with mixed gender
+    :param user_stats: dataset, containing all ego-alter pair info
+    :param l: int, long
+    :param w: int, width
+    :param n_bins: int, how many bins shown in the plot
+    :param mode: string, see from seaborn, available,'talk', 'notebook',
+    'paper', 'poster'.
+    :return: None
+    """
+    user_stats_M = user_stats[user_stats['Gender_Guesser_y'] == 'M']
+    user_stats_F = user_stats[user_stats['Gender_Guesser_y'] == 'F']
+
+    pred_alters_M = user_stats_M.groupby('userid_x').tail(1)['Pi_alters'].dropna()
+    pred_alters_ego_M = user_stats_M.groupby('userid_x').tail(1)['Pi_ego_alters'].dropna()
+    pred_alters_F = user_stats_F.groupby('userid_x').tail(1)['Pi_alters'].dropna()
+    pred_alters_ego_F = user_stats_F.groupby('userid_x').tail(1)['Pi_ego_alters'].dropna()
+
+    fig, ax = plt.subplots(figsize=(l, w))
+    sns.set_context(mode)
+
+    sns.distplot(pred_alters_M, label=r'$\Pi$: male alters only', bins=n_bins)
+    sns.distplot(pred_alters_ego_M, label=r'$\Pi$: ego + male alters', bins=n_bins)
+
+    sns.distplot(pred_alters_F, label=r'$\Pi$: female alters only', bins=n_bins)
+    sns.distplot(pred_alters_ego_F, label=r'$\Pi$: ego + female alters', bins=n_bins)
+
+    if user_stats_mixed is not None:
+        pred_alters_MF = user_stats_mixed.groupby('userid_x').tail(1)['Pi_alters'].dropna()
+        pred_alters_ego_MF = user_stats_mixed.groupby('userid_x').tail(1)['Pi_ego_alters'].dropna()
+
+        sns.distplot(pred_alters_MF, label=r'$\Pi$: mixed alters only', bins=n_bins)
+        sns.distplot(pred_alters_ego_MF, label=r'$\Pi$: ego + mixed alters', bins=n_bins)
+
+        # plt.title('Entropy and Cross entropy')
+    ax.set(xlabel='Predictability $\Pi$', ylabel='Density')
+    ax.legend()
+    plt.show()
+
+
