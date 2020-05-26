8405b24cc66830ce22c822c6a0b90d2c3b1c6569
Magica_Chen <sxtpy2010@gmail.com>
2020-05-26 16:59:27 +0100

    add a function for uniq_LZ_entropy

--- util.py
+++ util.py
@@ -112,3 +112,45 @@ def first_name_finder(s):
         return s[:id]
     else:
         return 'unknown'
+
+
+def uniq_LZ_entropy(seq, lambdas=False, e=200):
+    """Algorithm edited to have the option of taking a list of unique locations and finding
+    the users entropy with a sequence taking only values in the string "locs"
+
+    Returns Entropy
+
+    Edited to return 0 if not previously seen"""
+
+    """Estimate the entropy rate of the symbols encoded in `seq`, a list of
+    strings.
+
+    Kontoyiannis, I., Algoet, P. H., Suhov, Y. M., & Wyner, A. J. (1998).
+    Nonparametric entropy estimation for stationary processes and random
+    fields, with applications to English text. IEEE Transactions on Information
+    Theory, 44(3), 1319-1327.
+
+    Bagrow, James P., Xipei Liu, and Lewis Mitchell. "Information flow reveals
+    prediction limits in online social activity." Nature human behaviour 3.2
+    (2019): 122-128.
+    """
+    N = len(seq)
+
+    if N < e:
+        return np.nan
+    else:
+        L = []
+        for i, w in enumerate(seq):
+            prevSeq = " %s " % " ".join(seq[0:i])
+            seen = w in prevSeq
+            c = i
+            while seen and c < N:
+                c += 1
+                seen = (" %s " % " ".join(seq[i:c + 1])) in prevSeq
+            l = c - i
+            L.append(l)
+        wb = len([x for x in L if x != 0])
+        if lambdas:
+            return L
+        return (1.0 * wb / sum(L)) * np.log2(N - 1)
+
