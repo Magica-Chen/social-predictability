e999e11a6c181adbc9caf8d0afce3b203a889ea5
Magica_Chen <sxtpy2010@gmail.com>
2020-05-26 17:17:06 +0100

    add a function for uniq_LZ_cross_entropy

--- util.py
+++ util.py
@@ -149,8 +149,49 @@ def uniq_LZ_entropy(seq, lambdas=False, e=200):
                 seen = (" %s " % " ".join(seq[i:c + 1])) in prevSeq
             l = c - i
             L.append(l)
-        wb = len([x for x in L if x != 0])
+        wb = len([x for x in L if x > 0])
         if lambdas:
             return L
-        return (1.0 * wb / sum(L)) * np.log2(N - 1)
+        return (1.0 * wb / sum(L)) * np.log2(N)
 
+
+def uniq_LZ_cross_entropy(W1, W2, PTs, lambdas=False, e=100):
+    """
+    Only consider the seq seen in the previous, and other setting L[i]=0.
+    Find the cross entropy H_cross(W2|W1), how many bits we would need to
+    encode the data in W2 using the information in W1. W1 and W2 are lists of
+    strings, PTs is a list of integers with the same length as W2 denoting the
+    relative time ordering of W1 vs. W2. These integers tell us the position
+    PTs[x] = i in W1 such that all symbols in W1[:i] occurred before the x-th
+    word in W2.
+
+    Bagrow, James P., Xipei Liu, and Lewis Mitchell. "Information flow reveals
+    prediction limits in online social activity." Nature human behaviour 3.2
+    (2019): 122-128.
+    """
+
+    lenW1 = len(W1)
+    lenW2 = len(W2)
+
+    if lenW1 < e | lenW2 < e:
+        return np.nan
+    else:
+        L = []
+        for j, (wj, i) in enumerate(zip(W2, PTs)):
+            prevW1 = " %s " % " ".join(W1[:i])
+            seen = wj in prevW1
+            c = j
+            while seen and c < lenW2:
+                c += 1
+                seen = (" %s " % " ".join(W2[j:c]) in prevW1)
+            l = c - j
+            L.append(l)
+
+        wb = len([x for x in L if x > 0])
+        if lambdas:
+            return L
+
+        if sum(L) == wb:
+            return np.nan
+        else:
+            return (1.0 * wb / sum(L)) * np.log2(lenW1)
