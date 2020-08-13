2d34e26c9031d663c6964df2f6f36cf7ea21bc1c
Magica_Chen <sxtpy2010@gmail.com>
2020-08-13 23:54:52 +0100

    remove plot in class and add name for CV

--- meetup_strategy.py
+++ meetup_strategy.py
@@ -800,234 +800,6 @@ class MeetupOneByOne(Meetup):
     def meetup_details(self):
         return pd.concat([self.find_meetup_details(ego) for ego in self.egolist])
 
-    def hist_entropy(self, l=12, w=6, n_bins=100, mode='talk'):
-        """ Histogram plot for entropy and more
-        :param l: int, long
-        :param w: int, width
-        :param n_bins: int, how many bins shown in the plot
-        :param mode: string, see from seaborn, available,'talk', 'notebook',
-        'paper', 'poster'.
-        :return: None
-        """
-        LZentropy = self.ego_stats['LZ_entropy'].dropna()
-        CrossEntropy = self.user_stats['CE_alter'].dropna()
-        CrossEntropyEgo = self.user_stats['CCE_ego_alter'].dropna()
-        CumCrossEntropy = self.ego_stats['CCE_alters'].dropna()
-        CumCrossEntropyEgo = self.ego_stats['CCE_ego_alters'].dropna()
-
-        fig, ax = plt.subplots(figsize=(l, w))
-        sns.set_context(mode)
-        sns.distplot(LZentropy, label='LZ Entropy', bins=n_bins)
-        sns.distplot(CrossEntropy, label='Cross Entropy: alter only', bins=n_bins)
-        sns.distplot(CrossEntropyEgo, label='Cumulative Cross Entropy: ego + alter', bins=n_bins)
-        sns.distplot(CumCrossEntropy, label='Cumulative Cross Entropy: alters only', bins=n_bins)
-        sns.distplot(CumCrossEntropyEgo, label='Cumulative Cross Entropy: ego + alters', bins=n_bins)
-        # plt.title('Entropy and Cross entropy')
-        ax.set(xlabel='Entropy (bits)', ylabel='Density')
-        ax.legend(loc='upper left', bbox_to_anchor=(1.04, 1))
-        plt.show()
-
-    def hist_pred(self, l=12, w=6, n_bins=100, mode='talk'):
-        """ Histogram plot for predictability and theirs
-        :param l: int, long
-        :param w: int, width
-        :param n_bins: int, how many bins shown in the plot
-        :param mode: string, see from seaborn, available,'talk', 'notebook',
-        'paper', 'poster'.
-        :return: None
-        """
-        pred = self.ego_stats['Pi'].dropna()
-        pred_alter = self.user_stats['Pi_alter'].dropna()
-        pred_alter_ego = self.user_stats['Pi_ego_alter'].dropna()
-        pred_alters = self.ego_stats['Pi_alters'].dropna()
-        pred_alters_ego = self.ego_stats['Pi_ego_alters'].dropna()
-
-        fig, ax = plt.subplots(figsize=(l, w))
-        sns.set_context(mode)
-        sns.distplot(pred, label=r'$\Pi$: ego', bins=n_bins)
-        sns.distplot(pred_alter, label=r'$\Pi$: alter only', bins=n_bins)
-        sns.distplot(pred_alter_ego, label=r'$\Pi$: ego + alter', bins=n_bins)
-        sns.distplot(pred_alters, label=r'$\Pi$: alters only', bins=n_bins)
-        sns.distplot(pred_alters_ego, label=r'$\Pi$: ego + alters', bins=n_bins)
-        # plt.title('Entropy and Cross entropy')
-        ax.set(xlabel='Predictability $\Pi$', ylabel='Density')
-        ax.legend()
-        plt.show()
-
-    def paper_hist(self, l=15, w=6, n_bins=100, mode='talk',
-                   figsave=False, format='eps'):
-        """ Histogram plot for entropy and predictability given by the James' paper
-        :param l: int, long
-        :param w: int, width
-        :param n_bins: int, how many bins shown in the plot
-        :param mode: string, see from seaborn, available,'talk', 'notebook',
-        'paper', 'poster'.
-        :param figsave: whether the figure will be saved
-        :param format: png, eps, pdf, and so on.
-        :return: None
-        """
-        LZentropy = self.ego_stats['LZ_entropy'].dropna()
-        pred = self.ego_stats['Pi'].dropna()
-        # only include the most frequent alter
-        CrossEntropy = self.user_stats[self.user_stats['Included Rank'] == 1]['CE_alter'].dropna()
-        pred_alter = self.user_stats[self.user_stats['Included Rank'] == 1]['Pi_alter'].dropna()
-
-        fig, [ax1, ax2] = plt.subplots(1, 2, figsize=(l, w))
-        sns.set_context(mode)
-        sns.distplot(LZentropy, label='Entropy', bins=n_bins, ax=ax1, kde=False)
-        sns.distplot(CrossEntropy, label='Cross Entropy', bins=n_bins, ax=ax1, kde=False)
-        ax1.set(xlabel='Entropy (bits)', ylabel='Count')
-        ax1.legend(loc='best')
-
-        sns.distplot(pred, label='Entropy', bins=n_bins, ax=ax2, kde=False)
-        sns.distplot(pred_alter, label='Cross Entropy', bins=n_bins, ax=ax2, kde=False)
-        ax2.set(xlabel='Predictability', ylabel='Counts')
-        ax2.legend(loc='best')
-        plt.tight_layout()
-        plt.show()
-
-        if figsave:
-            title = 'Hist entropy and Pi.' + format
-            fig.savefig(title, bbox_inches='tight')
-
-    def paper_interaction(self, threshold=None, interval=None, l=12, w=8, n_bins=200, mode='talk',
-                          figsave=False, format='eps'):
-        """ Number of meetupers of alter vs predictability
-        :param l: int, long
-        :param w: int, width
-        :param n_bins: int, how many bins shown in the plot
-        :param mode: string, see from seaborn, available,'talk', 'notebook',
-        'paper', 'poster'.
-        :param threshold: int, the largest number of alters included
-        :param interval: int, the interval shown in axis
-        :param figsave: whether the figure will be saved
-        :param format: png, eps, pdf, and so on.
-
-        :return: None
-        """
-        N_meetupers = self.user_stats.groupby('userid_x')['userid_y'].count().reset_index(name='count')
-        iterim = self.user_stats.merge(N_meetupers, left_on='userid_y', right_on='userid_x', how='left')
-        if threshold is None:
-            threshold = max(iterim['count'])
-
-        fig, (ax1, ax2) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [2, 1]}, sharex=True,
-                                       figsize=(l, w))
-        sns.set_context(mode)
-        sns.pointplot(x="count", y="Pi_alter", data=iterim[iterim['count'] <= threshold],
-                      ci=95, join=False, ax=ax1)
-        if interval is None:
-            interval = round(threshold / 20)
-        ax1.xaxis.set_major_locator(ticker.MultipleLocator(interval))
-        ax1.xaxis.set_major_formatter(ticker.ScalarFormatter())
-        vals = ax1.get_yticks()
-        ax1.set_yticklabels(['{:,.2%}'.format(x) for x in vals])
-        ax1.set(ylabel='Predictability', xlabel='')
-        ax1.set_rasterized(True)
-
-        sns.distplot(iterim[iterim['count'] <= threshold]['count'], bins=n_bins, kde=False, ax=ax2)
-        ax2.set(xlabel='Number of meetupers of alter', ylabel='Count')
-
-        fig.tight_layout()
-        plt.show()
-
-        if figsave:
-            title = 'Meetupers of Alter vs Pi.' + format
-            fig.savefig(title, bbox_inches='tight')
-
-    def num_point_plot(self, name, threshold=None, interval=None, l=15, w=6, mode='talk',
-                       control=False, figsave=False, format='eps'):
-        """ number of included alters vs entropy or predictability
-        :param name: string, currently only accept 'entropy' or 'predictability'
-        :param threshold: int, the largest number of alters included
-        :param interval: int, the interval shown in axis
-        :param l: int, long
-        :param w: int, width
-        :param mode: string, see from seaborn, available,'talk', 'notebook',
-        'paper', 'poster'.
-        :param control: whether add temporal control and social control in the plot
-        :param figsave: whether the figure will be saved
-        :param format: png, eps, pdf, and so on.
-
-        :return: None
-        """
-        fig, ax = plt.subplots(figsize=(l, w))
-        sns.set_context(mode)
-
-        if name is 'entropy':
-            if control:
-                CCE = pd.melt(self.user_stats_all, id_vars=['Included Rank'],
-                              value_vars=['CCE_ego_alters', 'CCE_alters',
-                                          'CCE_ego_alters_tr', 'CCE_alters_tr',
-                                          'CCE_ego_alters_sr', 'CCE_alters_sr'],
-                              var_name='CCE')
-                baseline = self.ego_stats_all['LZ_entropy'].mean()
-                CCE_legend = ['Ego only', 'Alters + ego', 'Alters only',
-                              'Alters + ego (TC)', 'Alters only (TC)',
-                              'Alters + ego (SC)', 'Alters only (SC)']
-                if threshold is None:
-                    threshold = len(set(self.user_stats_all['Included Rank'].tolist()))
-            else:
-                CCE = pd.melt(self.user_stats, id_vars=['Included Rank'],
-                              value_vars=['CCE_ego_alters', 'CCE_alters'],
-                              var_name='CCE')
-                baseline = self.ego_stats['LZ_entropy'].mean()
-                CCE_legend = ['Ego only', 'Alters + ego', 'Alters only']
-                if threshold is None:
-                    threshold = len(set(self.user_stats['Included Rank'].tolist()))
-
-        elif name is 'predictability':
-            if control:
-                CCE = pd.melt(self.user_stats_all, id_vars=['Included Rank'],
-                              value_vars=['Pi_ego_alters', 'Pi_alters',
-                                          'Pi_ego_alters_tr', 'Pi_alters_tr',
-                                          'Pi_ego_alters_sr', 'Pi_alters_sr'
-                                          ],
-                              var_name='CCE')
-                baseline = self.ego_stats_all['Pi'].mean()
-                CCE_legend = ['Ego only', 'Alters + ego', 'Alters only',
-                              'Alters + ego (TC)', 'Alters only (TC)',
-                              'Alters + ego (SC)', 'Alters only (SC)']
-                if threshold is None:
-                    threshold = len(set(self.user_stats_all['Included Rank'].tolist()))
-
-            else:
-                CCE = pd.melt(self.user_stats, id_vars=['Included Rank'],
-                              value_vars=['Pi_ego_alters', 'Pi_alters'],
-                              var_name='CCE')
-                baseline = self.ego_stats['Pi'].mean()
-                CCE_legend = ['Ego only', 'Alters + ego', 'Alters only']
-                if threshold is None:
-                    threshold = len(set(self.user_stats['Included Rank'].tolist()))
-
-        else:
-            raise ValueError('Only available for entropy and predictability')
-
-        sns.pointplot(x="Included Rank", y="value", hue='CCE',
-                      data=CCE[CCE['Included Rank'] <= threshold],
-                      ci=95, join=False, ax=ax)
-        ax.axhline(y=baseline, color='black', linestyle='--', label='Ego')
-        leg_handles = ax.get_legend_handles_labels()[0]
-
-        if interval is None:
-            interval = round(threshold / 20)
-        ax.xaxis.set_major_locator(ticker.MultipleLocator(interval))
-        ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
-
-        if name is 'predictability':
-            vals = ax.get_yticks()
-            ax.set_yticklabels(['{:,.2%}'.format(x) for x in vals])
-            ax.legend(leg_handles, CCE_legend)
-            ax.set(xlabel='Number of included alters', ylabel='Predictability')
-        elif name is 'entropy':
-            ax.legend(leg_handles, CCE_legend, loc='upper left', bbox_to_anchor=(1.04, 1))
-            ax.set(xlabel='Number of included alters', ylabel='Cumulative Cross Entropy')
-
-        plt.show()
-
-        if figsave:
-            title = name + '-point.' + format
-            fig.savefig(title, bbox_inches='tight')
-
 
 class MeetupWhole(Meetup):
     """
@@ -1397,7 +1169,7 @@ class MeetupCrossValid(MeetupWhole):
 
     def __init__(self, path, mins_records=150, freq='H',
                  geoid=False, resolution=None, epsilon=2,
-                 n_previous=200,
+                 n_previous=200, name='wp',
                  user_meetup=None, total_meetup=None, placeidT=None):
         """
         All inputs are from the father class MeetupWhole and MeetupOneByOne
@@ -1409,6 +1181,7 @@ class MeetupCrossValid(MeetupWhole):
                                                total_meetup,
                                                placeidT)
         self.cross_info = None
+        self.name = name
 
     def _submeetup(self, n_meetupers=10):
         whole_meetup = self.user_meetup.groupby('userid_x')['userid_y'].count().reset_index(
@@ -1417,7 +1190,7 @@ class MeetupCrossValid(MeetupWhole):
         self.egolist = list(set(user_meetup['userid_x'].tolist()))
         return self.egolist
 
-    def ego_info(self, n_shown=10, verbose=False, filesave=False, name='wp'):
+    def ego_info(self, n_shown=10, verbose=False, filesave=False):
         """
         Combine all egos and all time delays as a dataframe
         :param name: string
@@ -1433,7 +1206,7 @@ class MeetupCrossValid(MeetupWhole):
         self.cross_info = ego_alters
 
         if filesave:
-            name = name + '_MeetupCrossValidation.csv'
+            name = self.name + '_CV.csv'
             self.cross_info.to_csv(name, index=False)
 
         return self.cross_info
