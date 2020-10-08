a005ad41bc90d48c971120e01ca3ebcfcb353820
Magica_Chen <sxtpy2010@gmail.com>
2020-10-08 02:16:38 +0100

    R:update plots

--- ggplot_for_report.R
+++ ggplot_for_report.R
@@ -6,6 +6,7 @@ library("reshape2")
 library("latex2exp")
 library("boot")
 library("scales")
+library("magrittr")
 
 theme_set(
   theme_bw() +
@@ -62,13 +63,21 @@ ggsave(
 # all_final <- read.csv('final/150_all_category_CP.csv')
 all_final <- read.csv('final/MeetupNp_Rank/150_all_category_MeetupNp.csv', stringsAsFactors = FALSE)
 # all_final <- read.csv('final/FreqNp_Rank/150_all_category_FreqNp.csv')
-all_final$included <- as.factor(all_final$included)
-all_final <- all_final %>% filter(category %in% c("CB-1H-MFN", 'TFN'))
 
-all_final$category[all_final$category=="CB-1H-MFN"]<-"Co-locatorship"
+# # only for part -----------------------------------
+# all_final <- all_final %>% filter(category %in% c("CB-1H-MFN", 'TFN'))
+# all_final$category[all_final$category=="CB-1H-MFN"]<-"Co-locatorship"
+# all_final$category[all_final$category=="TFN"]<-"Friendship"
+# only for all --------------------------------------
+all_final$category[all_final$category=="CB-1H-MFN"]<-"CB-1H-CN"
+all_final$category[all_final$category=="CB-1D-MFN"]<-"CB-1D-CN"
+all_final$category[all_final$category=="SW-24H-MFN"]<-"SW-24H-CN"
 all_final$category[all_final$category=="TFN"]<-"Friendship"
-all_final$category<- as.factor(all_final$category)
 
+all_final$category %<>% factor(levels= c("CB-1H-CN","Friendship","CB-1D-CN","SW-24H-CN"))
+##--------------------------------------------------
+all_final$category<- as.factor(all_final$category)
+all_final$included <- as.factor(all_final$included)
 
 ggplot(all_final, aes(x=included, y=mean, 
                       # shape=category,
@@ -102,9 +111,9 @@ ggplot(all_final, aes(x=included, y=mean,
 
 ggsave(
   # filename = "ALL_relative_Pi_CP.pdf", 
-  # filename = "ALL_relative_Pi_MeetupNp.pdf", 
+  filename = "ALL_relative_Pi_MeetupNp.pdf",
   # filename = "ALL_relative_Pi_FreqNp.pdf", 
-  filename = "ALL_relative_Pi_MeetupNp_part.pdf",
+  # filename = "ALL_relative_Pi_MeetupNp_part.pdf",
   device = "pdf",
   width = 9.90, height = 2.66,
   path = "fig/"
@@ -143,10 +152,9 @@ ggplot(all_final, aes(x=included, y=mean_CCE,
 
 ggsave(
   # filename = "ALL_relative_CCE_CP.pdf", 
-  # filename = "ALL_relative_CCE_MeetupNp.pdf", 
+  filename = "ALL_relative_CCE_MeetupNp.pdf",
   # filename = "ALL_relative_CCE_FreqNp.pdf", 
   # filename = "ALL_relative_CCE_MeetupNp_part.pdf",
-  filename = "ALL_relative_CCE_MeetupNp_part.pdf",
   device = "pdf",
   width = 9.90, height = 2.66,
   path = "fig/"
@@ -164,13 +172,16 @@ ggsave(
 # gw_vip_sim <- read.csv('final/gws-150/gws_VIP_similarity_user_CP.csv')
 # gw_vip_sim$dataset <- 'Gowalla'
 
-wp_vip_sim <- read.csv('final/wp-150/wp_VIP_similarity_user_MeetupNp.csv')
+wp_vip_sim <- read.csv('final/wp-150/wp_VIP_similarity_user_MeetupNp.csv', 
+                       stringsAsFactors = FALSE)
 wp_vip_sim$dataset <- 'Weeplace'
 
-bk_vip_sim <- read.csv('final/bk-150/bk_VIP_similarity_user_MeetupNp.csv')
+bk_vip_sim <- read.csv('final/bk-150/bk_VIP_similarity_user_MeetupNp.csv', 
+                       stringsAsFactors = FALSE)
 bk_vip_sim$dataset <- 'BrightKite'
 
-gw_vip_sim <- read.csv('final/gws-150/gws_VIP_similarity_user_MeetupNp.csv')
+gw_vip_sim <- read.csv('final/gws-150/gws_VIP_similarity_user_MeetupNp.csv', 
+                       stringsAsFactors = FALSE)
 gw_vip_sim$dataset <- 'Gowalla'
 
 # wp_vip_sim <- read.csv('final/wp-150/wp_VIP_similarity_user_FreqNp.csv')
@@ -183,88 +194,99 @@ gw_vip_sim$dataset <- 'Gowalla'
 # gw_vip_sim$dataset <- 'Gowalla'
 
 df_vip_sim <- do.call("rbind", list(wp_vip_sim, bk_vip_sim, gw_vip_sim))
-df_vip_sim <- df_vip_sim %>% filter(Compare == "CB-1H-MFN vs TFN")
 
-ggplot(df_vip_sim, aes(x=dataset, y=Jaccard,
-                       # shape=category,
-                       fill=dataset
-                       # color=Compare,
-)) +
-  geom_boxplot(width=0.5) +
-  theme(
-    legend.title = element_blank(),
-    axis.text.x = element_text(vjust = 0.5, hjust=0.5),
-    legend.position = "none",
-    # plot.title=element_text(face='bold', size=12,hjust = 0.5)
-  ) +  catscale10  + catscale10_2  +
-  labs(x='', y='Local Jaccard Similarity')
-
-ggsave(
-  filename = "VIP_similarity_user_MeetupNp_part.pdf", 
-  device = "pdf",
-  width = 2.45, height = 2.32,
-  path = "fig/"
-)
-
-
-dataset = c('BrightKite', 'Gowalla', 'Weeplace')
-sim = c(0.48, 0.53, 0.5)
-df_vip_sim_network <- data.frame(dataset, sim )
-ggplot(df_vip_sim_network, aes(x=dataset, y=sim, fill=dataset)) +
-  geom_bar(stat="identity", width=0.5) + 
-  theme(
-    legend.title = element_blank(),
-    axis.text.x = element_text(vjust = 0.5, hjust=0.5),
-    legend.position = "none",
-    # plot.title=element_text(face='bold', size=12,hjust = 0.5)
-  ) +  catscale10  + catscale10_2  +
-  labs(x='', y='Global Jaccard Similarity')
-
-ggsave(
-  filename = "VIP_similarity_MeetupNp_part.pdf", 
-  device = "pdf",
-  width = 2.45, height = 2.32,
-  path = "fig/"
-)
-
-# ggplot(df_vip_sim, aes(x=Compare, y=Jaccard,
-#                       # shape=category,
-#                       fill=Compare
-#                       # color=Compare,
-#                       )) +
-#   geom_boxplot() +
+# only for part ----------------------------------
+# df_vip_sim <- df_vip_sim %>% filter(Compare == "CB-1H-MFN vs TFN")
+# ggplot(df_vip_sim, aes(x=dataset, y=Jaccard,
+#                        # shape=category,
+#                        fill=dataset
+#                        # color=Compare,
+# )) +
+#   geom_boxplot(width=0.5) +
 #   theme(
 #     legend.title = element_blank(),
-#     axis.text.x = element_text(angle = 60, vjust = 1, hjust=1),
+#     axis.text.x = element_text(vjust = 0.5, hjust=0.5),
 #     legend.position = "none",
-#     strip.text = element_text(size = 15),
-#     legend.text = element_text(
-#       face = "bold",
-#       size = 12
-#     ),
-#     axis.text = element_text(
-#       face = "bold",
-#       size = 12
-#     ),
-#     axis.title = element_text(
-#       face = "bold",
-#       size = 12
-#     ),
 #     # plot.title=element_text(face='bold', size=12,hjust = 0.5)
-#   ) +  catscale10  + catscale10_2 +
-#   facet_wrap(~dataset, nrow = 3,
-#              strip.position = 'right',
-#              scales = "free_y") +
-#   labs(x='', y='Jaccard Similarity')
+#   ) +  catscale10  + catscale10_2  +
+#   labs(x='', y='Local Jaccard Similarity')
 # 
 # ggsave(
-#   # filename = "VIP_similarity_user_CP.pdf",
-#   # filename = "VIP_similarity_user_MeetupNp.pdf", 
-#   filename = "VIP_similarity_user_FreqNp.pdf", 
+#   filename = "VIP_similarity_user_MeetupNp_part.pdf", 
 #   device = "pdf",
-#   width = 10.5, height = 8.7,
+#   width = 2.45, height = 2.32,
 #   path = "fig/"
 # )
+# 
+# 
+# dataset = c('BrightKite', 'Gowalla', 'Weeplace')
+# sim = c(0.48, 0.53, 0.5)
+# df_vip_sim_network <- data.frame(dataset, sim )
+# ggplot(df_vip_sim_network, aes(x=dataset, y=sim, fill=dataset)) +
+#   geom_bar(stat="identity", width=0.5) + 
+#   theme(
+#     legend.title = element_blank(),
+#     axis.text.x = element_text(vjust = 0.5, hjust=0.5),
+#     legend.position = "none",
+#     # plot.title=element_text(face='bold', size=12,hjust = 0.5)
+#   ) +  catscale10  + catscale10_2  +
+#   labs(x='', y='Global Jaccard Similarity')
+# 
+# ggsave(
+#   filename = "VIP_similarity_MeetupNp_part.pdf", 
+#   device = "pdf",
+#   width = 2.45, height = 2.32,
+#   path = "fig/"
+# )
+# 
+# Only for all ---------------------------------
+
+df_vip_sim$Compare[df_vip_sim$Compare=="SW-24H-MFN vs CB-1D-MFN"]<-"SW-24H-CN vs CB-1D-CN"
+df_vip_sim$Compare[df_vip_sim$Compare=="SW-24H-MFN vs CB-1H-MFN"]<-"SW-24H-CN vs CB-1H-CN"
+df_vip_sim$Compare[df_vip_sim$Compare=="SW-24H-MFN vs TFN"]<-"SW-24H-CN vs Friendship"
+df_vip_sim$Compare[df_vip_sim$Compare=="CB-1D-MFN vs CB-1H-MFN"]<-"CB-1D-CN vs CB-1H-CN"
+df_vip_sim$Compare[df_vip_sim$Compare=="CB-1D-MFN vs TFN"]<-"CB-1D-CN vs Friendship"
+df_vip_sim$Compare[df_vip_sim$Compare=="CB-1H-MFN vs TFN"]<-"CB-1H-CN vs Friendship"
+df_vip_sim$Compare<- as.factor(df_vip_sim$Compare)
+
+ggplot(df_vip_sim, aes(x=Compare, y=Jaccard,
+                      # shape=category,
+                      fill=Compare
+                      # color=Compare,
+                      )) +
+  geom_boxplot() +
+  theme(
+    legend.title = element_blank(),
+    axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
+    legend.position = "none",
+    strip.text = element_text(size = 15),
+    legend.text = element_text(
+      face = "bold",
+      size = 12
+    ),
+    axis.text = element_text(
+      face = "bold",
+      size = 12
+    ),
+    axis.title = element_text(
+      face = "bold",
+      size = 12
+    ),
+    # plot.title=element_text(face='bold', size=12,hjust = 0.5)
+  ) +  catscale10  + catscale10_2 +
+  facet_wrap(~dataset, nrow = 3,
+             strip.position = 'right',
+             scales = "free_y") +
+  labs(x='', y='Jaccard Similarity')
+
+ggsave(
+  # filename = "VIP_similarity_user_CP.pdf",
+  filename = "VIP_similarity_user_MeetupNp.pdf",
+  # filename = "VIP_similarity_user_FreqNp.pdf",
+  device = "pdf",
+  width = 10.5, height = 8.7,
+  path = "fig/"
+)
 
 
 #----plot user USLR and SLR--------
@@ -314,10 +336,19 @@ ggsave(
 #----plot user cumulative SDLR-------
 vip_CLR <- read.csv('final/MeetupNp_Rank/150_all_cumulative_LR_MeetupNp.csv', 
                     stringsAsFactors = FALSE)
-vip_CLR <- vip_CLR %>% filter((LR == "USLR") & (category %in% c("CB-1H-MFN", 'TFN')))
-vip_CLR$category[vip_CLR$category=="CB-1H-MFN"]<-"Co-locatorship"
-vip_CLR$category[vip_CLR$category=="TFN"]<-"Friendship"
+# only for part ---------------------
+# vip_CLR <- vip_CLR %>% filter((LR == "USLR") & (category %in% c("CB-1H-MFN", 'TFN')))
+# vip_CLR$category[vip_CLR$category=="CB-1H-MFN"]<-"Co-locatorship"
+# vip_CLR$category[vip_CLR$category=="TFN"]<-"Friendship"
 
+# only for all ----------------------
+vip_CLR$category[vip_CLR$category=="CB-1H-MFN"]<-"CB-1H-CN"
+vip_CLR$category[vip_CLR$category=="CB-1D-MFN"]<-"CB-1D-CN"
+vip_CLR$category[vip_CLR$category=="SW-24H-MFN"]<-"SW-24H-CN"
+vip_CLR$category[vip_CLR$category=="TFN"]<-"Friendship"
+vip_CLR <- vip_CLR %>% filter(LR == "USLR")
+vip_CLR$category %<>% factor(levels= c("CB-1H-CN","Friendship","CB-1D-CN","SW-24H-CN"))
+# -----------------------------------
 vip_CLR$included <- as.factor(vip_CLR$included)
 vip_CLR$category<- as.factor(vip_CLR$category)
 
@@ -353,7 +384,8 @@ ggplot(vip_CLR, aes(x=included, y=mean,
   )
 
 ggsave(
-  filename = "VIP_cumulative_LR_MeetupNp.pdf", 
+  # filename = "VIP_cumulative_LR_MeetupNp.pdf", 
+  filename = "VIP_cumulative_LR_MeetupNp_all.pdf", 
   device = "pdf",
   width = 9.90, height = 2.66,
   path = "fig/"
@@ -362,7 +394,7 @@ ggsave(
 
 #-------plot for CV----------------------
 
-vip_CV <- read.csv('final/150_all_CV.csv')
+vip_CV <- read.csv('final/MeetupNp_Rank/150_all_CV_MeetupNp.csv')
 vip_CV$included <- as.factor(vip_CV$included)
 
 ggplot(vip_CV, aes(x=included, y=mean, 
@@ -387,6 +419,7 @@ ggplot(vip_CV, aes(x=included, y=mean,
     ),
     # plot.title=element_text(face='bold', size=12,hjust = 0.5)
   ) +  catscale10 + 
+  scale_y_continuous(labels = scales::percent) + 
   geom_errorbar(aes(ymin=lower, ymax=upper), width=.2,
                 position=position_dodge(0.05)) + 
   facet_wrap(~dataset, 
@@ -396,7 +429,7 @@ ggplot(vip_CV, aes(x=included, y=mean,
        )
 
 ggsave(
-  filename = "VIP_CV.pdf", device = "pdf",
+  filename = "VIP_CV_MeetupNp.pdf", device = "pdf",
   width = 9.90, height = 2.66,
   path = "fig/"
 )
